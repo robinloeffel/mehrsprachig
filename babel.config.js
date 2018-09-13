155b07c780976c47cb0e338118466f93726f2dea
Robin Löffel <robin.loeffel@unic.com>
2018-09-13 16:19:54 +0200

    :bulb: disable babel for now

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,11 @@
-export default {
+module.exports = {
     presets: [
         ['@babel/preset-env', {
             targets: {
                 ie: 11,
                 browsers: 'last 2 versions'
             },
-            useBuiltIns: 'usage',
-            modules: false,
-            debug: true
+            useBuiltIns: 'usage'
         }]
     ],
     ignore: [ 'node_modules' ]
--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,18 @@
 import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+// import resolve from 'rollup-plugin-node-resolve';
+// import commonjs from 'rollup-plugin-commonjs';
+// import { uglify } from 'rollup-plugin-uglify';
 import { eslint } from 'rollup-plugin-eslint';
-import pkg from './package.json';
 
 // merge the basic config that is the same for all
 // with the one tailored to the respective output
 const mergeConfigs = config => Object.assign(config, {
     dir: 'dist',
-    sourcemap: true,
-    plugins: [ resolve(), commonjs(), eslint(), babel() ]
+    sourcemap: true
 });
 
 export default {
-    input: pkg.index,
+    input: 'src/mehrsprachig.js',
     output: [ mergeConfigs({
         file: 'mehrsprachig.es.js',
         format: 'es'
@@ -21,5 +20,6 @@ export default {
         name: 'Mehrsprachig',
         file: 'mehrsprachig.iife.js',
         format: 'iife'
-    }) ]
+    }) ],
+    plugins: [ eslint() ]
 };
