4369e262900fa0a66fe8e36d46a18d37bce35e25
Robin Löffel <r.loeffel@outlook.com>
2018-09-18 15:57:21 +0200

    :package: update to newest rollup-plugin-babel and tell babel to export source maps

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,38 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-// import { uglify } from 'rollup-plugin-uglify';
+import { uglify } from 'rollup-plugin-uglify';
 import { eslint } from 'rollup-plugin-eslint';
 
-// merge the basic config that is the same for all
-// with the one tailored to the respective output
-const mergeConfigs = config => Object.assign(config, {
-    dir: 'dist',
-    sourcemap: true
-});
-
-export default {
+// two separate objects for two separate outputs
+// firstly esnext for bundlers, then umd for browsers
+export default [{
     input: 'src/mehrsprachig.js',
-    output: [ mergeConfigs({
+    plugins: [
+        eslint(),
+        resolve(),
+        commonjs()
+    ],
+    output: {
+        dir: 'dist',
+        sourcemap: true,
         file: 'mehrsprachig.es.js',
         format: 'es'
-    }), mergeConfigs({
-        name: 'Mehrsprachig',
-        file: 'mehrsprachig.umd.js',
-        format: 'umd'
-    }) ],
+    }
+}, {
+    input: 'src/mehrsprachig.js',
     plugins: [
         eslint(),
         resolve(),
         commonjs(),
-        babel()
-    ]
-};
+        babel(),
+        uglify()
+    ],
+    output: {
+        dir: 'dist',
+        sourcemap: true,
+        name: 'Mehrsprachig',
+        file: 'mehrsprachig.umd.js',
+        format: 'umd'
+    }
+}];
