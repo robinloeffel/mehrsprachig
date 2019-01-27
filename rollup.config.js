125c628562795de9bc0904c515c6dc73b34f0c94
Robin Löffel <r.loeffel@outlook.com>
2019-01-27 09:30:35 +0100

    :package: switch from uglify to terser, update deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import { uglify } from 'rollup-plugin-uglify';
+import { terser } from 'rollup-plugin-terser';
 import { eslint } from 'rollup-plugin-eslint';
 import pkg from './package.json';
 
@@ -26,7 +26,7 @@ export default [{
         resolve(),
         commonjs(),
         babel(),
-        uglify()
+        terser()
     ],
     output: {
         sourcemap: true,
